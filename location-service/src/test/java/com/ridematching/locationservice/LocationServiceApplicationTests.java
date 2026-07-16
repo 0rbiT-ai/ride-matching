b43feaf2803829ba67492b8a40ac92cec634f270
0rbiT-ai <akhilesh6249@gmail.com>
2026-07-17 00:00:25 +0530

    initial commit with dtos in location-service

---
+++ location-service/src/test/java/com/ridematching/locationservice/LocationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridematching.locationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
